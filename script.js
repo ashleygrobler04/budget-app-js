1c6c2e1fea3e7cc554e13d7467d27efc933313b0
ashleygrobler04 <ashleygrobler04@gmail.com>
2022-08-19 09:23:02 +0200

    Code refactor: reduce array to get total spent instead of for loop.

--- script.js
+++ script.js
@@ -3,80 +3,74 @@ let expenses = [];
 let income = document.getElementById("income").value;
 const btnCalculate = document.getElementById("Calculate");
 let totalSpent = document.getElementById("total");
-
-getTotalSpent = (arr) => {
-    let amount = 0;
-    for (let i = 0; i < arr.length; i++) {
-        amount += Number(arr[i].price);
-    }
-    return amount;
-};
+let total;
 
 addExpense = (expense, price, category) => {
-    let rowCount = tabel.rows.length;
-    let expenseObject = new Object();
-    expenseObject.expense = expense;
-    expenseObject.price = price;
-    expenseObject.category = category;
-    expenses.push(expenseObject);
-    //add the expense and price fields to the tabel and add a 'remove' button with id 'remove'
-    let r = tabel.insertRow(rowCount);
-    r.insertCell(0).textContent = expenses[rowCount].expense;
-    r.insertCell(1).textContent = expenses[rowCount].price;
-    r.insertCell(2).textContent = expenses[rowCount].category;
-    const btnRemove = document.createElement("button");
-    btnRemove.setAttribute("id", "remove");
-    btnRemove.innerHTML = "Remove";
-    btnRemove.addEventListener("click", () => {
-        let selectedRow = r.rowIndex - 1;
-        expenses.splice(selectedRow, 1);
-        tabel.deleteRow(selectedRow);
-        totalSpent.innerHTML = getTotalSpent(expenses);
-    });
-    r.insertCell(3).append(btnRemove);
+  let rowCount = tabel.rows.length;
+  let expenseObject = new Object();
+  expenseObject.expense = expense;
+  expenseObject.price = Number(price);
+  expenseObject.category = category;
+  expenses.push(expenseObject);
+  //add the expense and price fields to the tabel and add a 'remove' button with id 'remove'
+  let r = tabel.insertRow(rowCount);
+  r.insertCell(0).textContent = expenses[rowCount].expense;
+  r.insertCell(1).textContent = expenses[rowCount].price;
+  r.insertCell(2).textContent = expenses[rowCount].category;
+  const btnRemove = document.createElement("button");
+  btnRemove.setAttribute("id", "remove");
+  btnRemove.innerHTML = "Remove";
+  btnRemove.addEventListener("click", () => {
+    let selectedRow = r.rowIndex - 1;
+    expenses.splice(selectedRow, 1);
+    tabel.deleteRow(selectedRow);
+    totalSpent.innerHTML = `${total}`;
+  });
+  r.insertCell(3).append(btnRemove);
+  total = expenses.reduce((prev, cur) => prev + cur.price, 0);
 };
 
 isInRange = (value, arr) => {
-    //value refers to a total of numbers added together. Arr is the array that gets checked (getTotalSpent returns the sum of all expense prices in the array.)
-    return getTotalSpent(expenses) <= value;
+  //value refers to a total of numbers added together. Arr is the array that gets checked (getTotalSpent returns the sum of all expense prices in the array.)
+  return total <= value;
 };
 
 btnCalculate.addEventListener("click", () => {
-    const cat = document.getElementById("category").value;
-    addExpense(
-        document.getElementById("expense").value,
-        document.getElementById("price").value,
-        cat
-    );
-    document.getElementById("totals").innerHTML = "";
-    for (let i of [
-        "Savings",
-        "Housing",
-        "Transportation",
-        "Food",
-        "Utilities",
-        "Insurance",
-        "Medical",
-        "Personal spending",
-        "Recreation",
-        "Miscellaneous",
-    ]) {
-        document.getElementById(
-            "totals"
-        ).innerHTML += `<li>${i}: ${addTotalByCategory(i)}</li>`;
-    }
-    let status = isInRange(document.getElementById("income").value, expenses)
-        ? "in Range"
-        : "Out of range";
-    totalSpent.innerHTML = `${status} Spent ${getTotalSpent(expenses)}`;
+  const cat = document.getElementById("category").value;
+  addExpense(
+    document.getElementById("expense").value,
+    document.getElementById("price").value,
+    cat
+  );
+  document.getElementById("totals").innerHTML = "";
+  for (let i of [
+    "Savings",
+    "Housing",
+    "Transportation",
+    "Food",
+    "Utilities",
+    "Insurance",
+    "Medical",
+    "Personal spending",
+    "Recreation",
+    "Miscellaneous",
+  ]) {
+    document.getElementById(
+      "totals"
+    ).innerHTML += `<li>${i}: ${addTotalByCategory(i)}</li>`;
+  }
+  let status = isInRange(document.getElementById("income").value, expenses)
+    ? "in Range"
+    : "Out of range";
+  totalSpent.innerHTML = `${status} Spent ${total}`;
 });
 
 addTotalByCategory = (category) => {
-    var total = 0;
-    for (let i of expenses) {
-        if (i.category === category) {
-            total += Number(i.price);
-        }
+  var total = 0;
+  for (let i of expenses) {
+    if (i.category === category) {
+      total += Number(i.price);
     }
-    return total;
+  }
+  return total;
 };
