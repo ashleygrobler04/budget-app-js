92bf28fd34086423ad0e9ed2e04439de7440b600
ashleygrobler04 <ashleygrobler04@gmail.com>
2022-12-07 19:02:33 +0200

    Started working on an object oriented approach

---
+++ app.js
@@ -0,0 +1,13 @@
+class App {
+    constructor() {
+        this.expenses = [];
+    }
+}
+
+class Expense {
+    constructor(name, price, category) {
+        this.name = name;
+        this.price = price;
+        this.category = category;
+    }
+}
